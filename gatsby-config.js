2fd650dc8b68b077aab86217d8f528eec5764c42
xiang_w10_2020 <xiangfr007@gmail.com>
2020-12-28 02:10:11 +0100

    fix warning

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    'less-plugin-clean-css',
     `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-plugin-less`,
